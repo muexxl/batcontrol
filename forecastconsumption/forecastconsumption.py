3ab134df70a24f312f456050c41ebf411c9eeaa8
Matthias Strubel <matthias.strubel@aod-rpg.de>
2024-11-07 11:34:07 +0100

    Better convert via numpy

--- forecastconsumption/forecastconsumption.py
+++ forecastconsumption/forecastconsumption.py
@@ -71,8 +71,9 @@ class ForecastConsumption(object):
                 energy = df['energy'].median()
             prediction[h]=energy*self.scaling_factor
         
-        logger.debug('[FC Cons] predicting consumption: %s', ', '.join(f'{v:.2f}' for v in prediction.values()))
+        logger.debug('[FC Cons] predicting consumption: %s', np.array(list(prediction.values())).round(1))
         return prediction
+
     
     def get_annual_value(self):
         self.dataframe['energy'].sum()*8760/2016/1000
