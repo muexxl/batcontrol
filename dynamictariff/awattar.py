3a036fa97fa54a59886ee5b01947f1c5149c1a81
Matthias Strubel <matthias.strubel@aod-rpg.de>
2024-11-18 21:03:04 +0100

    Pylint improvements for dynamictariff modules (#55)
    * tibber.py - pylint improvements & timeout on requests.posts
    * evcc.py - pylint recommendations
    * awattar.py - pylint recommendations
    * dynamictariff.py - pylint recommendations
    * baseclass.py - pylint recommendations

--- dynamictariff/awattar.py
+++ dynamictariff/awattar.py
@@ -1,10 +1,38 @@
+"""Awattar Class
+
+This module implements the Awattar API to retrieve dynamic electricity prices.
+It inherits from the DynamicTariffBaseclass.
+
+Classes:
+    Awattar: A class to interact with the Awattar API and process electricity prices.
+
+Methods:
+    __init__(self,
+                timezone, country: str,
+                price_fees: float,
+                price_markup: float,
+                vat: float,
+                min_time_between_API_calls=0):
+
+        Initializes the Awattar class with the specified parameters.
+
+    get_raw_data_from_provider(self):
+        Fetches raw data from the Awattar API.
+
+    get_prices_from_raw_data(self):
+        Processes the raw data to extract and calculate electricity prices.
+"""
 import datetime
 import math
 import requests
 from .baseclass import DynamicTariffBaseclass
 
 class Awattar(DynamicTariffBaseclass):
-    def __init__(self, timezone ,country:str, price_fees:float, price_markup:float, vat:float,  min_time_between_API_calls=0):
+    """ Implement Awattar API to get dynamic electricity prices
+        Inherits from DynamicTariffBaseclass
+    """
+
+    def __init__(self, timezone ,country:str, min_time_between_API_calls=0):
         super().__init__(timezone,min_time_between_API_calls)
         country= country.lower()
         if country in ['at','de']:
@@ -12,15 +40,20 @@ class Awattar(DynamicTariffBaseclass):
         else:
             raise RuntimeError(f'[Awattar] Country Code {country} not known')
 
+        self.vat=0
+        self.price_fees=0
+        self.price_markup=0
+
+    def set_price_parameters(self, vat:float, price_fees:float, price_markup:float):
+        """ Set the extra price parameters for the tariff calculation """
         self.vat=vat
         self.price_fees=price_fees
         self.price_markup=price_markup
 
     def get_raw_data_from_provider(self):
-        response=requests.get(self.url)
-
+        response=requests.get(self.url, timeout=30)
         if response.status_code != 200:
-           raise RuntimeError(f'[Awattar_AT] API returned {response}')
+            raise RuntimeError(f'[Awattar_AT] API returned {response}')
 
         raw_data=response.json()
         return raw_data
@@ -31,10 +64,13 @@ class Awattar(DynamicTariffBaseclass):
         now=datetime.datetime.now().astimezone(self.timezone)
         prices={}
         for item in data:
-            timestamp=datetime.datetime.fromtimestamp(item['start_timestamp']/1000).astimezone(self.timezone)
+            timestamp=datetime.datetime.fromtimestamp(
+                            item['start_timestamp']/1000).astimezone(self.timezone
+                        )
             diff=timestamp-now
             rel_hour=math.ceil(diff.total_seconds()/3600)
             if rel_hour >=0:
-                end_price=(item['marketprice']/1000*(1+self.price_markup)+self.price_fees)*(1+self.vat)
+                end_price=( item['marketprice']/1000*(1+self.price_markup) + self.price_fees
+                          ) * (1+self.vat)
                 prices[rel_hour]=end_price
         return prices
--- dynamictariff/dynamictariff.py
+++ dynamictariff/dynamictariff.py
@@ -1,10 +1,26 @@
+"""
+DynamicTariff class to select and configure a dynamic tariff provider based
+     on the given configuration.
 
+Args:
+    config (dict): Configuration dictionary containing the provider type and necessary parameters.
+    timezone (str): Timezone information.
+    min_time_between_API_calls (int): Minimum time interval between API calls.
+
+Returns:
+    selected_tariff: An instance of the selected tariff provider class (Awattar, Tibber, or Evcc).
+
+Raises:
+    RuntimeError: If required fields are missing in the configuration
+                     or if the provider type is unknown.
+"""
 from .awattar import Awattar
 from .tibber import Tibber
 from .evcc import Evcc
 
-class DynamicTariff(object):
-    def __new__(cls,  config:dict, timezone,min_time_between_API_calls):
+class DynamicTariff:
+    """ Select and configure a dynamic tariff provider based on the given configuration """
+    def __new__(cls,  config:dict, timezone,min_time_between_API_calls):  # pylint: disable=invalid-name
         selected_tariff=None
         provider=config['type']
 
@@ -12,31 +28,44 @@ class DynamicTariff(object):
             required_fields=['vat', 'markup', 'fees']
             for field in required_fields:
                 if not field in config.keys():
-                    raise RuntimeError(f'[DynTariff] Please include {field} in your configuration file')
+                    raise RuntimeError(
+                        f'[DynTariff] Please include {field} in your configuration file'
+                    )
             vat = float(config['vat'])
             markup = float(config['markup'])
             fees = float(config['fees'])
-            selected_tariff= Awattar(timezone,'at',fees,markup,vat,min_time_between_API_calls)
+            selected_tariff= Awattar(timezone,'at',min_time_between_API_calls)
+            selected_tariff.set_price_parameters(vat,fees,markup)
 
         elif provider.lower()=='awattar_de':
             required_fields=['vat', 'markup', 'fees']
             for field in required_fields:
                 if not field in config.keys():
-                    raise RuntimeError(f'[DynTariff] Please include {field} in your configuration file')
+                    raise RuntimeError(
+                        f'[DynTariff] Please include {field} in your configuration file'
+                    )
             vat = float(config['vat'])
             markup = float(config['markup'])
             fees = float(config['fees'])
-            selected_tariff= Awattar(timezone,'de',fees,markup,vat,min_time_between_API_calls)
+            selected_tariff= Awattar(timezone,'de',min_time_between_API_calls)
+            selected_tariff.set_price_parameters(vat,fees,markup)
 
         elif provider.lower()=='tibber':
             if not 'apikey' in config.keys() :
-                raise RuntimeError (f'[Dynamic Tariff] Tibber requires an API token. Please provide "apikey :YOURKEY" in your configuration file')
+                raise RuntimeError (
+                    '[Dynamic Tariff] Tibber requires an API token. '
+                    'Please provide "apikey :YOURKEY" in your configuration file'
+                    )
             token = config['apikey']
             selected_tariff=Tibber(timezone,token,min_time_between_API_calls)
 
         elif provider.lower()=='evcc':
             if not 'url' in config.keys() :
-                raise RuntimeError (f'[Dynamic Tariff] EVCC requires an URL. Please provide "url" in your configuration file, like http://evcc.local/api/tariff/grid')
+                raise RuntimeError (
+                    '[Dynamic Tariff] EVCC requires an URL. '
+                    'Please provide "url" in your configuration file, '
+                    'like http://evcc.local/api/tariff/grid'
+                    )
             selected_tariff= Evcc(timezone,config['url'],min_time_between_API_calls)
         else:
             raise RuntimeError(f'[DynamicTariff] Unkown provider {provider}')
