dd2db53385aa81d30e8671f9972201c3f1706503
Stephan Muekusch <stephan.muekusch@gmail.com>
2025-01-23 14:02:02 +0100

    fix entrypoint_ha.sh

--- entrypoint_ha.sh
+++ entrypoint_ha.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-SHARED_FOLDER="/batcontrol/addon_config"
-CONFIG_FOLDER="/batcontrol/config"
+SHARED_FOLDER="/app/addon_config"
+CONFIG_FOLDER="/app/config"
 
 CONFIG_FILE="$CONFIG_FOLDER/batcontrol_config.yaml"
 CONFIG_FILE_SHARED="$SHARED_FOLDER/batcontrol_config.yaml"
@@ -12,7 +12,7 @@ LOAD_PROFILE="$CONFIG_FOLDER/load_profile.csv"
 LOAD_PROFILE_DEFAULT="$CONFIG_FOLDER/load_profile_default.csv"
 LOAD_PROFILE_SHARED="$SHARED_FOLDER/load_profile.csv"
 
-LOGFILE="/data/batcontrol.log"
+LOG_FILE="/data/batcontrol.log"
 
 # use custom config file, if available
 if test -e $CONFIG_FILE_SHARED ; then
@@ -34,10 +34,11 @@ fi
 
 # Check if logfile exists. If not
 # Create a symlink to /app/log/batcontrol.log
-if test ! -e $LOGFILE ; then
-  touch $LOGFILE
-  ln -sf $LOGFILE /app/logs/batcontrol.log
+if test ! -e $LOG_FILE ; then
+  echo "Creating log file at $LOG_FILE"
+  touch $LOG_FILE
 fi
+ln -sf $LOG_FILE /app/logs/batcontrol.log
 
 # Start batcontrol.py
 exec python batcontrol.py
