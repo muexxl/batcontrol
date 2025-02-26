4485c82dd755c1c9b44027343ef7fcc4f65fbc55
Matthias Strubel <matthias.strubel@aod-rpg.de>
2025-02-26 20:13:23 +0100

    evcc: fix max_charge_limit comparison logic

--- evcc_api.py
+++ evcc_api.py
@@ -186,7 +186,7 @@ class EvccApi():
             self.old_allow_discharge_limit = self.get_always_allow_discharge_limit_function()
         if self.old_max_charge_limit is None:
             self.old_max_charge_limit = self.get_max_charge_limit_function()
-            if self.old_max_charge_limit > self.evcc_batterSoc:
+            if self.old_max_charge_limit > self.evcc_batterSoc / 100 :
                 # Only store if the old value is higher than the new one,
                 # which will may be altered by batcontrol to a lower value.
                 self.old_max_charge_limit = None
