129f3da8b5b172f641faefc316ac855e8e8d6950
Julfried <julian.grimm8@gmail.com>
2025-03-31 22:22:32 +0200

    Rename logfilelimiter module to logs

--- src/batcontrol/core.py
+++ src/batcontrol/core.py
@@ -16,7 +16,7 @@ from .evcc_api import EvccApi
 
 from .dynamictariff import DynamicTariff as tariff_factory
 from .inverter import Inverter as inverter_factory
-from .logfilelimiter import LogFileLimiter
+from .logs import LogFileLimiter
 
 from .forecastsolar import ForecastSolar as solar_factory
 
